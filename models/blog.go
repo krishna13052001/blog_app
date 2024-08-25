8f2e244f22d91aa3216a45522d7c72ac6ce48afe
avx-jkrishna <kjonnalagadda@aviatrix.com>
2024-08-25 21:59:46 +0530

    updating the blog structure

--- models/blog.go
+++ models/blog.go
@@ -4,8 +4,10 @@ type Blog struct {
 	ID        string `json:"id" bson:"_id"`
 	Title     string `json:"title" bson:"title"`
 	Body      string `json:"body" bson:"body"`
-	CreatedBy string `json:"createdBy" bson:"createdBy"`
-	CreatedAt int64  `json:"createdAt" bson:"createdAt"`
+	Batch     string `json:"batch" bson:"batch"`
+	JobType   string `json:"jobType" bson:"jobType"`
+	Location  string `json:"location" bson:"location"`
+	PayRange  string `json:"payRange" bson:"payRange"`
+	ApplyLink string `json:"applyLink" bson:"applyLink"`
 	UpdatedAt int64  `json:"updatedAt" bson:"updatedAt"`
-	UpdatedBy string `json:"updatedBy" bson:"updatedBy"`
 }
